1185381d19bc8f2200e3f3d1e3b960f94359e60b
Shaik <AbdulSalam.Shaik@bs.nttdata.com>
2023-11-16 22:30:01 +0530

    Remove numbers from string program

--- src/practiceJava/RemoveNumsInString.java
+++ src/practiceJava/RemoveNumsInString.java
@@ -1,10 +1,25 @@
 package practiceJava;
 
+import java.util.Scanner;
+
 public class RemoveNumsInString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		public static String removeAllDigit(String str)
+	    {
+	        char[] charArray = str.toCharArray();
+	        String result = "";
+	        for (int i = 0; i < charArray.length; i++) {
+	            if (!Character.isDigit(charArray[i])) {
+	                result = result + charArray[i];
+	            }
+	        }
+	        return result;
+	    }
+		
+	    public static void main(String args[])
+	    {   
+	    	Scanner sc = new Scanner(System.in);
+	        String str = sc.next();
+	        System.out.println(removeAllDigit(str));
+	    }
 }
